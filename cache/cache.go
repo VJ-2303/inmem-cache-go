e272b027a34e366c88249d8fc1b68cbc6242fe4b
VJ-2303 <vanaraj1018@gmail.com>
2026-01-03 18:37:58 +0530

    Created Cache struct in the cache package which hold the map and the mutex, and Added an new function for returning an new cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,1 +1,18 @@
 package cache
+
+import "sync"
+
+// Cache represents an basic cache data structure
+// it contains an Map to contains the data and an
+// mutex to allow concurrent access without any dataraces
+type Cache[K comparable, V any] struct {
+	items map[K]V    // Storing the key-value pairs
+	mu    sync.Mutex // Controlling concurrent access
+}
+
+// New creates a new cache instance
+func New[K comparable, V any]() *Cache[K, V] {
+	return &Cache[K, V]{
+		items: make(map[K]V),
+	}
+}
