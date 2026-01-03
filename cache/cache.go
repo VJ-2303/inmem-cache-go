cee296bbb96aca2c251180580e2260ae238e92c7
VJ-2303 <vanaraj1018@gmail.com>
2026-01-03 18:50:40 +0530

    Added CRUD Functions for cache struct

--- cache/cache.go
+++ cache/cache.go
@@ -16,3 +16,41 @@ func New[K comparable, V any]() *Cache[K, V] {
 		items: make(map[K]V),
 	}
 }
+
+// Set adds or update a key-value pair in the cache
+func (c *Cache[K, V]) Set(key K, value V) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.items[key] = value
+}
+
+// Get returns the value associated with the given key
+// from the cache. the bool value will return true if no matching key is found
+func (c *Cache[K, V]) Get(key K) (V, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	value, found := c.items[key]
+	return value, found
+}
+
+// Remove deletes the key-value with the specified key from the cache
+func (c *Cache[K, V]) Remove(key K) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.items, key)
+}
+
+func (c *Cache[K, V]) Pop(key K) (V, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	value, found := c.items[key]
+
+	if found {
+		delete(c.items, key)
+	}
+	return value, found
+}
